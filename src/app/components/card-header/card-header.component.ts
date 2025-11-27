2e2f54a1d0b107d609dc2a3816c4786259966c5d
leonardo.barros <leonardombr@hotmail.com>
2025-11-27 19:31:52 -0300

    Ajuste pedidos

---
+++ src/app/components/card-header/card-header.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { NgIf } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { TemPermissaoDirective } from 'src/app/diretivas/tem-permissao.directive';
+import { MatDivider } from "@angular/material/divider";
+
+@Component({
+  selector: 'app-card-header',
+  standalone: true,
+  imports: [
+    RouterLink,
+    NgIf,
+    MatButtonModule,
+    MatIconModule,
+    TemPermissaoDirective,
+    MatDivider
+],
+  templateUrl: './card-header.component.html',
+  styleUrls: ['./card-header.component.scss'],
+})
+export class CardHeaderComponent {
+  @Input() titulo!: string;
+
+  @Input() botaoTexto?: string;
+  @Input() botaoIcone: string = 'add';
+  @Input() botaoRota?: string | any[];
+  @Input() mostrarDivisor: boolean = false;
+
+  // cor do botão, default = 'primary'
+  @Input() botaoCor: 'primary' | 'accent' | 'warn' = 'primary';
+
+  // compatível com a sua directive (sem `?`)
+  @Input() permissao: string | string[] = '';
+}
